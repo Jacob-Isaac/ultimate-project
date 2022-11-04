9db2736ccdb269aad4c3162d3432f935d86bb559
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-11-04 22:49:41 +0100

    button temporaly not used

--- src/features/GlobePage/GlobeComponent/index.js
+++ src/features/GlobePage/GlobeComponent/index.js
@@ -19,9 +19,9 @@ const Globe = () => (
 disable-tap 
 auto-rotate 
   > 
-   <button class="view-button" slot="hotspot-0" data-position="-0.0569m 0.0969m -0.1398m" data-normal="-0.5829775m 0.2863482m -0.7603565m" data-orbit="-50.94862deg 84.56856deg 0.06545582m" data-target="-0.04384604m 0.07348397m -0.1213202m">
+   {/* <button class="view-button" slot="hotspot-0" data-position="-0.0569m 0.0969m -0.1398m" data-normal="-0.5829775m 0.2863482m -0.7603565m" data-orbit="-50.94862deg 84.56856deg 0.06545582m" data-target="-0.04384604m 0.07348397m -0.1213202m">
     The Fighters
-  </button>      
+  </button>       */}
   </model-viewer>
 
 </Model>
